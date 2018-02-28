6dd692a0c7838406f34217cb98b6e165fedbfa1d
home <home@home>
2018-02-27 22:09:35 -0600

    Test Jenkins Commit

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -5,7 +5,12 @@ import models.User;
 public class LoginController {
     private String loginView;
     private User userModel;
-    
+
+    /**
+     * s
+     * @param loginView
+     * @param userModel
+     */
     public LoginController(String loginView, User userModel) {
 
         this.loginView = loginView;
